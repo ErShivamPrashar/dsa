391598dd61330a53cab83f3a6cdd45570507e901
shivam <codewithshivam24@gmail.com>
2025-06-29 20:31:21 +0530

    Add functions to delete nodes from singly linked list: first, last, and by position

---
+++ LinkedList/singly/delete-first.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+
+
+// self referential structure
+typedef struct node{
+    int data;
+    struct node* next;
+}SLL; // SLL denotes singly list list program
+
+SLL* head = NULL;
+
+
+void deletefirst(){
+    SLL *temp = head;
+    if(head == NULL){
+        puts("list is empty\n");
+        return;
+    }
+    head = temp->next;
+    printf("%d is deleted\n",temp->data);
+    free(temp);
+}
+
+
+//insert value at the first position
+void insertFirst(int val){
+SLL* ptr = (SLL*)malloc(sizeof(SLL));
+
+ptr->data = val;
+ptr->next = NULL;
+
+if (head == NULL)
+{
+   head = ptr;
+   return;
+}
+
+ptr->next = head;
+head = ptr;
+
+}
+
+// display linked list data
+void display(){
+SLL* temp = head;
+int i=1;
+while (temp!=NULL)
+{
+    printf("node %d Data = %d\n",i,temp->data);
+    i++;
+   temp = temp->next;
+}
+
+}
+
+//main function to the singly linkedlist
+int main(){
+    system("color D0");
+    //insert value call
+    insertFirst(10);
+    insertFirst(20);
+    insertFirst(30);
+    insertFirst(40);
+    insertFirst(50);
+   
+    // display value call
+    display();
+
+    deletefirst();
+
+      display();
+getch();
+    return 0;
+}
+
---
+++ LinkedList/singly/delete-last.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+
+// self referential structure
+typedef struct node{
+    int data;
+    struct node* next;
+}SLL; // SLL denotes singly list list program
+
+SLL* head = NULL;
+
+
+
+void deletelast(){
+    SLL *temp = head;
+        SLL *p = temp->next;
+    if(head == NULL){
+        puts("list is empty\n");
+        return;
+    }
+    while (temp->next->next!=NULL)
+    {
+    temp = temp->next;
+    p = p->next;
+    }
+
+    printf("%d is deleted\n",p->data);
+    temp->next = NULL;
+    free(p);
+    
+}
+
+
+//insert value at the last position
+void insertLast(int val){
+SLL* ptr = (SLL*)malloc(sizeof(SLL));
+SLL* temp;
+ptr->data = val;
+ptr->next = NULL;
+
+if (head == NULL)
+{
+   head = ptr;
+   return;
+}
+temp = head;
+while (temp->next!=NULL)
+{ 
+   temp = temp->next;
+}
+
+temp->next = ptr;
+
+}
+
+// display linked list data
+void display(){
+SLL* temp = head;
+int i=1;
+while (temp!=NULL)
+{
+    printf("node %d Data = %d\n",i,temp->data);
+    i++;
+   temp = temp->next;
+}
+
+}
+
+//main function to the singly linkedlist
+int main(){
+    //insert value call
+    system("color D0");
+    insertLast(10);
+    insertLast(20);
+    insertLast(30);
+    insertLast(40);
+    insertLast(50);
+   
+    // display value call
+    display();
+
+    deletelast();
+
+    display();
+getch();
+    return 0;
+}
+
